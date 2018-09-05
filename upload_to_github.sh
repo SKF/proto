a8a86b5990175dcaa976872c8034341ca4d61206
Fredrik Lönnblad <fredrik+github@lonnblad.com>
2018-09-05 16:45:17 +0200

    Fixed the deploy issue (#10)

--- upload_to_github.sh
+++ upload_to_github.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 set -e
 
+services="hierarchy iam iot pas"
+
 # $1 language
+echo "language: $1"
 # $2 branch
+echo "branch: $2"
 # $3 files
+echo "files: $3"
 
 setup_git() {
   git config --global user.email "deploy@travis-ci.org"
@@ -12,14 +17,21 @@ setup_git() {
 
 commit_files() {
   git checkout -b $2
-  git add $3
-  git commit --message "Deploy SKF/proto to github.com/SKF/proto.git:$2"
+
+  for i in $( ls $3 ); do
+    if [[ $services =~ (^|[[:space:]])$i($|[[:space:]]) ]]; then
+      cp -rf $3/$i .
+      git add -v $i
+    fi
+  done
+
+  git commit -v -m "Deploy SKF/proto to github.com/SKF/proto.git:$2"
   git tag "${TRAVIS_TAG}-$1"
 }
 
 upload_files() {
   git remote add origin https://${GITHUB_TOKEN}@github.com/SKF/proto.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin $1 
+  git push -v --set-upstream origin $1 
 }
 
 setup_git
