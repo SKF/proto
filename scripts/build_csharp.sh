edbadec420fc59396f4fda418dc7f31194f4da20
Fredrik Lönnblad <fredrik+github@lonnblad.com>
2018-09-06 18:30:11 +0200

    Cleaning/common proto and removed python (#18)
    * Removed python generation and added a common proto pkg
    
    * Fixed the deploy script as well

--- scripts/build_csharp.sh
+++ scripts/build_csharp.sh
@@ -3,6 +3,14 @@ TOOLS=$(ls -d build/csharp_project/packages/grpc.tools/*/tools/linux_x64/)
 PROTOC=$TOOLS/protoc
 PLUGIN=$TOOLS/grpc_csharp_plugin
 
+mkdir -p build/csharp/common;
+$PROTOC \
+  --csharp_out build/csharp/common \
+  --grpc_out build/csharp/common \
+  --plugin protoc-gen-grpc=$PLUGIN \
+  common/common.proto;
+ls -lR build/csharp/common
+
 for SERVICE in $SERVICES; do
   mkdir -p build/csharp/$SERVICE;
   $PROTOC \
